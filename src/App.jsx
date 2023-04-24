764d8d69f2e1fd7e42a87ffc2a6210e8803acd6e
chetanmanoj <chetanmanoj2001@gmail.com>
2023-04-25 02:05:51 +0530

    Final initialization

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='h-screen w-screen bg-red-300'>
+
+    </div>
+  )
+}
+
+export default App
